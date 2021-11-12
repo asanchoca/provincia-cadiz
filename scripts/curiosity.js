7ec3be426017705e611eac2d3e91044b7240497c
asanchoca <79749679+asanchoca@users.noreply.github.com>
2021-11-12 21:12:16 +0100

    Add files via upload

---
+++ scripts/curiosity.js
@@ -0,0 +1,7 @@
+function mostrarCuriosidadCarnaval() {
+    document.getElementById("curiosidad").innerHTML = "Las primeras referencias del siglo XVI, exponen que en tiempos de carnaval, las gaditanas arrancaban las flores de las macetas para lanzárselas unos a otros a modo de broma. ";
+}
+
+function mostrarCuriosidadVinos() {
+  document.getElementById("curiosidad").innerHTML = "En las Bodegas Fundador de Jerez de la Frontera, hay una firma de Lola Flores en una barrica de vino, datada el 08/04/1952.";
+}
